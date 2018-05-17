7353c40269d081d44509459c6dd40c57cf3f3606
Joe Nagel <jnagel88@gmail.com>
2018-05-17 13:47:33 -0500

    integrated Index action for portfolio items.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+          title: "my blog post #{blog}",
+          body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  )
+end
+
+5.times do |skill|
+  Skill.create!(
+           title: "Rails #{skill}",
+           percent_utilized: 15
+  )
+end
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+           title: "portfolio title #{portfolio_item}" ,
+           subtitle: "portfolio subtitle #{portfolio_item}",
+           body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+           main_image: "https://placeholdit.co//i/600x400",
+           thumb_image: "https://placeholdit.co//i/350x200"
+  )
+end
