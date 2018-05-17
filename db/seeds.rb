0c6f27a9bc569a14e2b7ef5d7f5cb6a3727b793b
Joe Nagel <jnagel88@gmail.com>
2018-05-17 15:36:53 -0500

    added edit buttons for portfolio items.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,22 @@
 
 10.times do |blog|
   Blog.create!(
-          title: "my blog post #{blog}",
+          title: "my blog post #{blog +1}",
           body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   )
 end
 
 5.times do |skill|
   Skill.create!(
-           title: "Rails #{skill}",
+           title: "Rails #{skill + 1}",
            percent_utilized: 15
   )
 end
 
 9.times do |portfolio_item|
   Portfolio.create!(
-           title: "portfolio title #{portfolio_item}" ,
-           subtitle: "portfolio subtitle #{portfolio_item}",
+           title: "portfolio title #{portfolio_item + 1}" ,
+           subtitle: "portfolio subtitle #{portfolio_item + 1}",
            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
            main_image: "https://placeholdit.co//i/600x400",
            thumb_image: "https://placeholdit.co//i/350x200"
