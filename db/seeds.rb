e77b54a9906d1aed4efecd7d6dd0fe365bfadf0c
Joe Nagel <jnagel88@gmail.com>
2018-05-21 09:00:01 -0500

    updated broken 'placeholdit.co' links in dev DB, seeds and model concerns

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,8 +33,8 @@ end
            title: "portfolio title #{portfolio_item + 1}" ,
            subtitle: "Ruby on Rails",
            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-           main_image: "https://placeholdit.co//i/600x400",
-           thumb_image: "https://placeholdit.co//i/350x200"
+           main_image: "http://via.placeholder.com/600x400",
+           thumb_image: "http://via.placeholder.com/350x200"
   )
 end
 
