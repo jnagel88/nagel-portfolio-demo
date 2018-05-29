5d2e3ab81780ac2c84df8e3dfd33497ce24ab9f9
Joe Nagel <jnagel88@gmail.com>
2018-05-29 11:07:40 -0500

    integrated inital drag/drop function.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require html.sortable
-//= require turbolinks
-//= require_tree .
 //= require jquery3
 //= require jquery-ui
 //= require popper
 //= require bootstrap-sprockets
+//= require turbolinks
+//= require html.sortable
+//= require_tree .
+
