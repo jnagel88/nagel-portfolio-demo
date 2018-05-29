543830f02c388f48ce6044d60ee0ffe10f524a81
Joe Nagel <jnagel88@gmail.com>
2018-05-29 16:12:50 -0500

    implemented persistance of drag/drop interface.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  skip_before_action :verify_authenticity_token, :only => [:sort]
   before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout "portfolio"
 
@@ -7,7 +8,13 @@ class PortfoliosController < ApplicationController
 
   def index
     @portfolio_items = Portfolio.by_position
+  end
 
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+    head :ok
   end
 
   def angular
