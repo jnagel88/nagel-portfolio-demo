9b8f03b8765bd096e1fe56b6e7325542ba715f3a
Joe Nagel <jnagel88@gmail.com>
2018-05-31 18:54:51 -0500

    finished carrierwave installation.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
 
   # Optionally define an asset host for configurations that are fronted by a
   # content host, such as CloudFront.
-  config.asset_host = 'http://example.com'
+  # config.asset_host = 'http://example.com'
 
   # The maximum period for authenticated_urls is only 7 days.
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
@@ -20,7 +20,7 @@ CarrierWave.configure do |config|
       access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       region:            ENV.fetch('AWS_REGION'), # Required
-      stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
+      
   }
 
   # Optional: Signing of download urls, e.g. for serving private content through
