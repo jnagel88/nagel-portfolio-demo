430c69e1376461c7706790d53c9569ff9e363e75
Joe Nagel <jnagel88@gmail.com>
2018-05-31 16:05:06 -0500

    setup carrierwave to push to AWS S3.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,7 +4,7 @@ class Portfolio < ApplicationRecord
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
   include Placeholder
-  validates_presence_of :title, :body, :thumb_image, :main_image
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
