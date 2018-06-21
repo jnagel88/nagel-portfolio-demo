4dfcf7153da0796f7cd5b00f562c14f2dc51edf7
Joe Nagel <jnagel88@gmail.com>
2018-06-21 12:41:00 -0500

    implemented deployment gems and install process.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,31 +1,63 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+# config valid only for current version of Capistrano
+lock '3.11.0'
 
+set :repo_url, 'git@github.com:jnagel88/nagel-portfolio-demo.git'
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+set :user, 'deploy'
 set :application, 'nagel-portfolio'
-set :repo_url, 'git@github.com:jnagel88/nagel-portfolio-demo.git' # Edit this to match your repository
-set :branch, :master
-set :deploy_to, '/home/deploy/nagel-portfolio'
+set :rails_env, 'production'
+server '18.233.8.96', user: "#{fetch(:user)}", roles: %w{app db web}, primary: true
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :pty, true
-set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
-set :keep_releases, 5
-set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.4.1' # Edit this if you are using MRI Ruby
 
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 8]
-set :puma_workers, 0
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true
-set :puma_preload_app, false
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :config_example_suffix, '.example'
+set :config_files, %w{config/database.yml config/secrets.yml}
+set :puma_conf, "#{shared_path}/config/puma.rb"
+
+namespace :deploy do
+  before 'check:linked_files', 'config:push'
+  before 'check:linked_files', 'puma:config'
+  before 'check:linked_files', 'puma:nginx_config'
+  before 'deploy:migrate', 'deploy:db:create'
+  after 'puma:smart_restart', 'nginx:restart'
+end
+
+
+# --------------
+# config valid for current version and patch releases of Capistrano
+# lock "~> 3.11.0"
+
+# set :application, 'nagel-portfolio'
+# set :repo_url, 'git@github.com:jnagel88/nagel-portfolio-demo.git' # Edit this to match your repository
+# set :branch, :master
+# set :deploy_to, '/home/deploy/nagel-portfolio'
+# set :pty, true
+# set :linked_files, %w{config/database.yml config/application.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+# set :keep_releases, 5
+# set :rvm_type, :user
+# set :rvm_ruby_version, 'ruby-2.4.1' # Edit this if you are using MRI Ruby
+
+# set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+# set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+# set :puma_conf, "#{shared_path}/puma.rb"
+# set :puma_access_log, "#{shared_path}/log/puma_error.log"
+# set :puma_error_log, "#{shared_path}/log/puma_access.log"
+# set :puma_role, :app
+# set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+# set :puma_threads, [0, 8]
+# set :puma_workers, 0
+# set :puma_worker_timeout, nil
+# set :puma_init_active_record, true
+# set :puma_preload_app, false
+
+# ----------------------
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
