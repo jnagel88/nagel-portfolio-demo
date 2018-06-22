f33811a3cbf423dc92862de2ccbe20342038b5ae
Joe Nagel <jnagel88@gmail.com>
2018-06-21 20:36:57 -0500

    updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,11 +11,11 @@ server '18.233.8.96', user: "#{fetch(:user)}", roles: %w{app db web}, primary: t
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :pty, true
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb', 'config/application.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :config_example_suffix, '.example'
-set :config_files, %w{config/database.yml config/secrets.yml}
+set :config_files, %w{config/database.yml config/secrets.yml config/application.yml}
 set :puma_conf, "#{shared_path}/config/puma.rb"
 
 namespace :deploy do
