0e0993373e37b3945e71404cd2cd634d77a12c76
Joe Nagel <jnagel88@gmail.com>
2018-06-24 17:58:07 -0500

    implemented return to last page after successful sign-in.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -70,26 +70,15 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
-
   def alerts
     alert = (flash[:alert] || flash[:error] || flash[:notice])
-
     if alert
       alert_generator alert
     end
   end
 
-
-
-
   def alert_generator msg
-    js add_gritter(msg, title: "Joe Nagel's Portoflio", sticky: true)
+    js add_gritter(msg, title: "Joe Nagel's Portoflio")
   end
-
-
-
-
-
-
 
 end
